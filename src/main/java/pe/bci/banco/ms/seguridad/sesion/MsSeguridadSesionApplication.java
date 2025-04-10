488ab3a1ddc2f2d997d1a542f34b52e775f4dfc6
brosas <brosas@bch.bancodechile.cl>
2025-04-09 21:55:59 -0500

    project base

---
+++ src/main/java/pe/bci/banco/ms/seguridad/sesion/MsSeguridadSesionApplication.java
@@ -0,0 +1,13 @@
+package pe.bci.banco.ms.seguridad.sesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSeguridadSesionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSeguridadSesionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/bci/banco/ms/seguridad/sesion/MsSeguridadSesionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.bci.banco.ms.seguridad.sesion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeguridadSesionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
